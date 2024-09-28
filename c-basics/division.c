1d93ffc967f018fae6122c363a980add088f5316
netia-melvin <melvinatemba18@gmail.com>
2024-09-27 23:00:30 -0700

    division operator

---
+++ c-basics/division.c
@@ -0,0 +1,14 @@
+/*
+ *Change the variable type to double
+ *Change the result format too
+*/
+#include <stdio.h>
+int main(){
+	double x = 4.5;
+	double result = x/5;
+
+	printf("%.2lf", result);
+
+return 0;	
+
+}
