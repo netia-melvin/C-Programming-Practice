51b5b135f9d69e6d74045b5b7dd46b20d75aaa01
netia-melvin <melvinatemba18@gmail.com>
2024-09-27 22:10:39 -0700

    multi comments

---
+++ c-basics/multi-line-comments.c
@@ -0,0 +1,13 @@
+/*This is the first practice on c
+I'm learning on multiline comments
+Select and use ctrl shift forward slash
+It is really fun doing this*/
+
+#include <stdio.h>
+int main() {
+    double weight;
+    scanf("%lf", &weight);
+    printf("Your Weight = %.2lf", weight);
+    
+    return 0;
+}
