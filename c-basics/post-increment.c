0cc898a1e11f6a777d8b231be3ae31c7cc32cf25
netia-melvin <melvinatemba18@gmail.com>
2024-09-28 01:44:42 -0700

    fixes

---
+++ c-basics/post-increment.c
@@ -0,0 +1,9 @@
+/*the value of "x" will be used in the expression and then it will be increased by 1*/
+#include <stdio.h>
+int main() {
+  int x = 50;
+  int y = 10 + x++;
+  printf("x = %d, y = %d\n", x, y);
+  
+   return 0;
+}
