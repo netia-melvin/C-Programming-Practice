1ba3fbce6ce5e551243cd80681a1e517ea4c49ee
netia-melvin <melvinatemba18@gmail.com>
2024-09-27 22:03:21 -0700

    comments

---
+++ c-basics/comments.c
@@ -0,0 +1,9 @@
+// This is the first practice on c
+#include <stdio.h>
+int main() {
+    double weight;
+    scanf("%lf", &weight);
+    printf("Your Weight = %.2lf", weight);
+    
+    return 0;
+}
