0a3d5239b62132daa74a653022567a31747fa954
netia-melvin <melvinatemba18@gmail.com>
2024-09-28 00:20:43 -0700

    remainder operator

---
+++ c-basics/remainder.c
@@ -0,0 +1,15 @@
+/*
+ * Is the remainder result after division of two numbers
+ * Only used with integers
+ * Using with floats will give an error
+ *
+*/ 
+
+#include <stdio.h>
+int main() {
+	int x = 12;
+	int y = x%5;
+	printf("%d", y);	
+	
+return 0;
+}
